62a596b1b207f79cadf76598bb978c119240ba17
KinoFool <nicolas.mary@epitech.eu>
2022-11-15 21:10:19 +0100

    Add rss feed model

---
+++ backend/src/models/rss.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const rssFeedSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+const RssFeed = mongoose.model('RssFeed', rssFeedSchema);
+
+module.exports = RssFeed;
