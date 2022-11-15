1ddc00603269a202a6883591cb1df03dff0d6c28
KinoFool <nicolas.mary@epitech.eu>
2022-11-15 21:13:17 +0100

    rss feed rename title to name

--- backend/src/models/rss.js
+++ backend/src/models/rss.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const rssFeedSchema = new Schema({
-    title: {
+    name: {
         type: String,
         required: true
     },
