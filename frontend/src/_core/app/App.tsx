618bbef564e5390386368ebd837c672f5701dc44
filoupegase <loisoncorentin@yahoo.fr>
2022-11-15 17:16:11 +0100

    save theming

--- frontend/src/_core/app/App.tsx
+++ frontend/src/_core/app/App.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 function App() {
     return (
-        <div>
-            <h3>App/</h3>
+        <div className='main-content'>
+            <div style={ {position: "relative", height: '71px', background: 'aliceblue'} }>
+
+            </div>
+            <div
+                style={ {
+                    background: 'linear-gradient(rgb(248, 250, 253) 0%, rgba(248, 250, 253, 0) 413px)',
+                    height: '1200px'
+                } }
+            >
+
+            </div>
         </div>
     );
 }
