18b780c116847dcfd321d34cf383b999e6dd3058
KinoFool <nicolas.mary@epitech.eu>
2022-11-15 21:00:33 +0100

    Add user model

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-// const User = require('../models/user'); TODO Need to create a user moder
+const User = require('../models/user');
 
 // User signup
 passport.use('signup',
