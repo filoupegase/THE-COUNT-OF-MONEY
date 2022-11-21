cb591f0501a665db199b95146f43af28afa0ec51
KinoFool <nicolas.mary@epitech.eu>
2022-11-21 15:25:36 +0100

    Signup with an username

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -2,17 +2,31 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const UserModel = require('../models/user');
 
-// TODO
-
-// User signup
 passport.use('signup',
   new LocalStrategy({
       usernameField: 'email',
-      passwordField: 'password'
-    },
-    async (email, password, done) => {
+      passwordField: 'password',
+      passReqToCallback: true
+    }, async (req, email, password, done) => {
       try {
-        const user = await UserModel.create({email, password});
+        // Verify if the email has a good format
+        console.log("email: " + email);
+        if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/))
+          throw new Error("Email has a bad format")
+
+        // Verify if the email is already in use
+        if (await UserModel.findOne({email}))
+          throw new Error("Email is already in use")
+
+        // Verify if the username is already in use
+        if (await UserModel.findOne({username: req.body.username}))
+          throw new Error("Username is already in use")
+
+        const user = await UserModel.create({
+          email,
+          password,
+          username: req.body.username
+        });
         return done(null, user);
       } catch (error) {
         done(error);
