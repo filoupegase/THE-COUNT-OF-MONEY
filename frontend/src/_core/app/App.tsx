f5ec716474146e453b283b03fc426219775272e4
filoupegase <loisoncorentin@yahoo.fr>
2022-11-28 12:34:07 +0100

    feat/store store

--- frontend/src/_core/app/App.tsx
+++ frontend/src/_core/app/App.tsx
@@ -3,19 +3,17 @@ import Layout from "../../_common/component/Layout";
 import { Box } from '@mui/material';
 import DataGrid from "../../_common/component/DataGrid";
 import { useAppSelector, useAppDispatch } from '../store/hooks';
-import { increment, decrement } from "../store/counterSlice";
+import { increment, decrement, incrementByAmount } from "../store/counterSlice";
 
 function App() {
-    // The `state` arg is correctly typed as `RootState` already
     const count = useAppSelector((state) => state.counter.value);
     const dispatch = useAppDispatch();
-
-    console.log(count);
 
     return (
         <>
             <button onClick={ () => dispatch(increment()) } type='button'>+</button>
             <button onClick={ () => dispatch(decrement()) } type='button'>-</button>
+            <button onClick={ () => dispatch(incrementByAmount(4)) }>je sais pas</button>
             { count }
             <Box style={ {
                 background: 'linear-gradient(rgb(243 248 255) 0%, rgba(248, 250, 253, 0) 413px)',
