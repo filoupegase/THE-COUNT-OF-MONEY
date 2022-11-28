8ce30c3829b2fc096369e687fd81a3b7cea86efe
KinoFool <nicolas.mary@epitech.eu>
2022-11-28 13:01:47 +0100

    Get the X popular crypto

--- backend/src/crypto/coinmarketcap.js
+++ backend/src/crypto/coinmarketcap.js
@@ -3,13 +3,20 @@ const axios = require('axios');
 const basetestUrl = 'https://sandbox-api.coinmarketcap.com';
 const baseprodUrl = 'https://pro-api.coinmarketcap.com';
 
-
+/**
+ * Get the data from the API of coinmarketcap
+ *
+ * @param path the path of the API
+ * @returns {Promise<*>} the data (json)
+ */
 const getCoinmarketcapData = async (path) => {
   let response = null;
   try {
-    response = await axios.get(`${basetestUrl}${path}`, {
+    console.log('API Token: ' + process.env.CMC_API_KEY);
+    response = await axios.get(`${baseprodUrl}${path}`, {
       headers: {
-        'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c', // This is a fake token only valid for the sandbox
+        // 'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c', // This is a fake token only valid for the sandbox
+        'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
         'Accept-Encoding': 'null'
       },
     });
--- backend/src/routes/crypto.js
+++ backend/src/routes/crypto.js
@@ -12,4 +12,15 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// Get the popular cryptocurrencies
+router.get('/popular', async (req, res, next) => {
+  let limit = 10; // TODO use the value set by the admin
+  try {
+    const data = await coinMarketCap.getCoinmarketcapData(`/v1/cryptocurrency/listings/latest?limit=${limit}`);
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
