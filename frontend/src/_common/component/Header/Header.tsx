ce748e08caaea439f5460276a5b74e70a2cdb065
filoupegase <loisoncorentin@yahoo.fr>
2022-11-29 12:25:44 +0100

    clean able

--- frontend/src/_common/component/Header/Header.tsx
+++ frontend/src/_common/component/Header/Header.tsx
@@ -13,7 +13,7 @@ const BoxHeader = styled(Box)(() => ({
         height: 'auto',
         background: 'white',
         borderBottom: "1px solid #eff2f5",
-        padding: "8px clamp(16px, (100vw - 1280px) / 2, 9999px)",
+        padding: "8px clamp(16px, (100vw - 1200px) / 2, 9999px)",
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center'
--- frontend/src/business/Header/NavHeader/NavHeader.tsx
+++ frontend/src/business/Header/NavHeader/NavHeader.tsx
@@ -7,7 +7,7 @@ const NavHeader = () => {
     return (
         <>
             <Box sx={ { display: 'flex', alignItems: 'center' } }>
-                <DiamondIcon style={ { height: 38, width: 38, marginRight: 5 } } />
+                <DiamondIcon style={ { height: 32, width: 32, marginRight: 5 } } />
                 <Typography sx={ { fontWeight: 600, letterSpacing: '-1px' } } variant="h6">The Count of Money
                 </Typography>
             </Box>
