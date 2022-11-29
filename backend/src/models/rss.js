1258f2823f4ef13964cf88014e0f6f1b8d633904
KinoFool <nicolas.mary@epitech.eu>
2022-11-29 17:41:55 +0100

    Add rss

--- backend/src/models/rss.js
+++ backend/src/models/rss.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const rssFeedSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    link: {
-        type: String,
-        required: true
-    },
-    state: {
-        type: Boolean,
-        required: true,
-        default: false
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
 });
 
 const RssFeed = mongoose.model('RssFeed', rssFeedSchema);
--- backend/src/routes/admin/rss.js
+++ backend/src/routes/admin/rss.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const rssController = require('../controller/rss');
+const rssController = require('../../controller/rss');
 
 const router = express.Router();
 
+// All the rss routes that require Admin privileges
+
 // Get all the rss feeds in the database
 router.get('/', rssController.getRssFeeds);
+
 // Get a single rss feed by id
 router.get('/:id', rssController.getRssFeed);
-// Create a new rss feed, only admin can do this
+// Add a new rss feed
 router.post('/', rssController.createRssFeed)
 
 // Update an rss feed
@@ -16,3 +19,5 @@ router.post('/', rssController.createRssFeed)
 router.delete('/:id', rssController.deleteRssFeed);
 
 module.exports = router;
+
+
