bfda420849ce960e6d4dad0ce1e2f6a69658c723
filoupegase <loisoncorentin@yahoo.fr>
2022-12-05 14:35:50 +0100

    save datagrid

--- frontend/src/_common/component/DataGrid/DataGrid.tsx
+++ frontend/src/_common/component/DataGrid/DataGrid.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import { Typography } from '@mui/material';
+import { DataGrid, GridColDef, GridValueGetterParams, GridRenderCellParams } from '@mui/x-data-grid';
 
-const rows: GridRowsProp = [
+const rows = [
     { id: 1, name: "Hello", price: "World" },
     { id: 2, name: "MUI X", price: "is awesome" },
     { id: 3, name: "Material UI", price: "is amazing" },
@@ -14,9 +15,21 @@ const rows: GridRowsProp = [
     { id: 10, name: "MUI Base", price: "is amazing" }
 ];
 
+function getFullName(params: GridValueGetterParams) {
+    return `${ params.row.name || '' } ${ params.row.price || '' }`;
+}
+
+
 const columns: GridColDef[] = [
     { field: "id", headerName: "#", hide: false, },
-    { field: "name", headerName: "Name", width: 150 },
+    {
+        field: "name",
+        headerName: "Name",
+        width: 150,
+        renderCell: (params: GridRenderCellParams) => (
+            <Typography>{ params.row.name }</Typography>
+        )
+    },
     { field: "price", headerName: "Price", width: 150 },
 ];
 
@@ -25,16 +38,11 @@ const DataGridCmp = () => {
     return (
         <>
             <div style={ { marginTop: 40 } }></div>
-            <div style={ {
-                display: 'flex',
-            } }>
-                <DataGrid
-                    autoHeight
-                    rows={ rows }
-                    columns={ columns }
-                    getRowId={ (row) => row.id }
-                />
-            </div>
+            <DataGrid
+                autoHeight
+                rows={ rows }
+                columns={ columns }
+            />
         </>
     )
 };
