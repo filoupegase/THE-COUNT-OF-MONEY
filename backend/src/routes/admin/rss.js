8f2685a067a0fe4598ece62d5681378f6ee64287
KinoFool <nicolas.mary@epitech.eu>
2022-12-05 16:31:47 +0100

    Repair admin get rss

--- backend/src/routes/admin/rss.js
+++ backend/src/routes/admin/rss.js
@@ -6,10 +6,20 @@ const router = express.Router();
 // All the rss routes that require Admin privileges
 
 // Get all the rss feeds in the database
-router.get('/', rssController.getRssFeeds);
+router.get('/', (req, res, next) => {
+  rssController.getRssFeeds()
+    .then(rssFeeds => {
+      res.status(200).send(rssFeeds);
+    })
+    .catch(err => {
+      res.status(500);
+      return next(err);
+    });
+});
 
 // Get a single rss feed by id
 router.get('/:id', rssController.getRssFeed);
+
 // Add a new rss feed
 router.post('/', rssController.createRssFeed)
 
