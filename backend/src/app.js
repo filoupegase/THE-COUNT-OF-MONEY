f783f34fb2eb71370ab298ca812a79b1a7e8e084
KinoFool <nicolas.mary@epitech.eu>
2022-12-13 10:05:17 +0100

    Remove unused import

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
