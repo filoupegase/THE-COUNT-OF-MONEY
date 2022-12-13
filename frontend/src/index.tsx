6d9935897956d5f718d4b139e25183c2d36e646d
filoupegase <loisoncorentin@yahoo.fr>
2022-12-13 18:59:14 +0100

    supress all I dont need 🧞

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import App from './_core/app';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './_core/style/theme';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import { store } from './_core/_store/store';
 
 
@@ -23,7 +23,4 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
