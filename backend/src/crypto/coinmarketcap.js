bdf7762e478abb997760cac3606cfd367ea09d1d
KinoFool <nicolas.mary@epitech.eu>
2023-01-03 23:04:33 +0100

    [Back] Poular crypto more use the state

--- backend/src/crypto/coinmarketcap.js
+++ backend/src/crypto/coinmarketcap.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const basetestUrl = 'https://sandbox-api.coinmarketcap.com';
+// const basetestUrl = 'https://sandbox-api.coinmarketcap.com';
 const baseprodUrl = 'https://pro-api.coinmarketcap.com';
 
 /**
@@ -13,6 +13,7 @@ const getCoinmarketcapData = async (path) => {
   let response = null;
   try {
     response = await axios.get(`${baseprodUrl}${path}`, {
+    // response = await axios.get(`${basetestUrl}${path}`, {
       headers: {
         // 'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c', // This is a fake token only valid for the sandbox
         'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
