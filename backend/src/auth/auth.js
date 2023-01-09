ce4b04a362b82e0f683ca749d10d635755cb6991
KinoFool <nicolas.mary@epitech.eu>
2023-01-09 11:47:29 +0100

    [Unit test] Admin manage rss

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -91,6 +91,8 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
       callbackURL: 'http://localhost:4000/api/auth/google/callback',
+      // callbackURL: `http://${process.env.HOST_BACKEND || 'localhost'}:${process.env.PORT_BACKEND || '4000'}/auth/google/callback`,
+
       scope: ['profile', 'email'],
       passReqToCallback: true
     }, async (req, accessToken, refreshToken, profile, done) => {
